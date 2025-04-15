47b6c4db040cd63da3ba2dc112d88b4243b8d3bd
Pierre GRANGER <pgranger23@gmail.com>
2025-04-15 17:33:08 +0200

    ...

--- compiled/index.js
+++ compiled/index.js
@@ -13872,35 +13872,35 @@ try {
 
   // Get the payload from the GitHub context
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  var payloadBuf = Buffer.from(payload, 'utf8');
+  payload = JSON.parse(payload);
   console.log(`The event payload: ${payload}`);
 
-  const signature = 'sha1=' + crypto
-      .createHmac('sha1', webhookToken)
-      .update(payloadBuf)
-      .digest('hex');
+  const signature = crypto
+    .createHmac('sha1', webhookToken)
+    .update(JSON.stringify(payload))
+    .digest('hex');
 
   // Send the payload to the webhook with the signature
   const response = await (0,node_fetch__WEBPACK_IMPORTED_MODULE_0__/* ["default"] */ .Ay)(webhookUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-GitHub-Event': github.context.eventName,
-        'X-Hub-Signature': signature, // Include the signature in the headers
-      },
-      body: payloadBuf,
-    });
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-GitHub-Event': github.context.eventName,
+      'X-Hub-Signature': 'sha1='+signature, // Include the signature in the headers
+    },
+    body: payload,
+  });
 
-    // Check if the request was successful
-    if (!response.ok) {
-      core.setFailed(`Failed to send payload to webhook: ${response.statusText}`);
-    }
-    else {
-      core.info(`Payload sent to webhook successfully: ${response.status}`);
-    }
-  } catch (error) {
-    core.setFailed(error.message);
+  // Check if the request was successful
+  if (!response.ok) {
+    core.setFailed(`Failed to send payload to webhook: ${response.statusText}`);
   }
+  else {
+    core.info(`Payload sent to webhook successfully: ${response.status}`);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
 
 __webpack_async_result__();
 } catch(e) { __webpack_async_result__(e); } }, 1);
--- src/js/index.js
+++ src/js/index.js
@@ -9,32 +9,32 @@ try {
 
   // Get the payload from the GitHub context
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  var payloadBuf = Buffer.from(payload, 'utf8');
+  payload = JSON.parse(payload);
   console.log(`The event payload: ${payload}`);
 
-  const signature = 'sha1=' + crypto
-      .createHmac('sha1', webhookToken)
-      .update(payloadBuf)
-      .digest('hex');
+  const signature = crypto
+    .createHmac('sha1', webhookToken)
+    .update(JSON.stringify(payload))
+    .digest('hex');
 
   // Send the payload to the webhook with the signature
   const response = await fetch(webhookUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-GitHub-Event': github.context.eventName,
-        'X-Hub-Signature': signature, // Include the signature in the headers
-      },
-      body: payloadBuf,
-    });
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-GitHub-Event': github.context.eventName,
+      'X-Hub-Signature': 'sha1='+signature, // Include the signature in the headers
+    },
+    body: payload,
+  });
 
-    // Check if the request was successful
-    if (!response.ok) {
-      core.setFailed(`Failed to send payload to webhook: ${response.statusText}`);
-    }
-    else {
-      core.info(`Payload sent to webhook successfully: ${response.status}`);
-    }
-  } catch (error) {
-    core.setFailed(error.message);
+  // Check if the request was successful
+  if (!response.ok) {
+    core.setFailed(`Failed to send payload to webhook: ${response.statusText}`);
   }
+  else {
+    core.info(`Payload sent to webhook successfully: ${response.status}`);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
